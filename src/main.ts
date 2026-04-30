0f53d2c3dd78331566698f6cb128f3439b6edab0
ElishreeDey <elishreed@mindfiresolutions.com>
2026-04-30 17:53:25 +0530

    Feat: Diaply data when we refresh the screen

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
-import { renderApp } from "./components/App";
-import { initFormEvents } from "./components/Form";
-import { initTableEvents } from "./components/Table";
-
+import { renderApp } from "./components/App"; //render main App where Form,Table and Snackbar htmls are called
+import { initFormEvents } from "./components/Form"; // form events
+import { initTableEvents } from "./components/Table"; // table events
+import { displayData } from "./services/displayTable"; // display local stored data when we refresh.
 
 const root = document.getElementById("app")!;
 
 root.innerHTML = renderApp();
 
-initFormEvents();
-initTableEvents();
+displayData();// when we fresh the screen and any data in local storage it will display in Table
+initFormEvents(); //initialize and attach here so Form click events with will work
+initTableEvents(); //initialize and attach here so Table click events with will work
